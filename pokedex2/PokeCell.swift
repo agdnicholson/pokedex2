1fe3d9b2350c116a8d6d49892d2c188cbcf94388
Andrew Nicholson <anicholson@clicksco.com>
2015-12-14 11:37:19 +0000

    PokeCell created.

---
+++ pokedex2/PokeCell.swift
@@ -0,0 +1,22 @@
+//
+//  PokeCell.swift
+//  pokedex2
+//
+//  Created by Andrew Nicholson on 14/12/2015.
+//  Copyright © 2015 Nicholson Media. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl:  UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}

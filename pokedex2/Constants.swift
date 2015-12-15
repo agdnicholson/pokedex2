f3cfe6e4dab8460fd44d0a973502efeac6148a46
Andrew Nicholson <anicholson@clicksco.com>
2015-12-15 23:54:33 +0000

    Data parsing from api.

---
+++ pokedex2/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex2
+//
+//  Created by Andrew Nicholson on 15/12/2015.
+//  Copyright © 2015 Nicholson Media. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://www.pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
